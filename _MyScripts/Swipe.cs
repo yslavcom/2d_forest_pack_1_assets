1dea42e932e709db00c2a03742cb11896e11bcb5
yslavcom <yslavcom@gmail.com>
2019-03-03 12:06:40 +1100

    changes to Swipe.cs & _scene_1

--- _MyScripts/Swipe.cs
+++ _MyScripts/Swipe.cs
@@ -1,67 +1,87 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Swipe : MonoBehaviour {
 
-    public int Boundary = 125;
+#region TapStructure 
+    public struct StTap
+    {
+        private bool _tap;
+        public bool Tap
+        {
+            get { return _tap; }
+            set { _tap = value; }
+        }
 
-    private bool boTap, boSwiped;
+        private Vector2 _coord;
+        public Vector2 Coord
+        {
+            get { return _coord; }
+            set { _coord = value; }
+        }
+
+        public StTap(bool tap, Vector2 coord)
+        {
+            _tap = tap;
+            _coord = coord;
+        }
+    }
+#endregion
 
     [SerializeField]
-    private Vector2 vec2StartTouchTap;
+    private StTap tap = new StTap(false, Vector2.zero);
+
+    private bool boSwiped;
     private Vector2 vec2StartTouchTapSwipe;
 
     private void Start()
     {
-        boTap = boSwiped = false;
-        vec2StartTouchTap = vec2StartTouchTapSwipe = Vector2.zero;
-}
-
+    }
 
     private void Update()
     {
-        #region Standalone Inputs
+#region Standalone Inputs
         if(Input.GetMouseButtonDown(0))
         {
-            if (!boTap) vec2StartTouchTapSwipe = (Vector2)Input.mousePosition;
+            if (!tap.Tap) vec2StartTouchTapSwipe = (Vector2)Input.mousePosition;
 
-            boTap = true;
-            vec2StartTouchTap = Input.mousePosition;
+            tap.Tap = true;
+            tap.Coord = Input.mousePosition;
         }
         else if(Input.GetMouseButtonUp(0))
         {
-            if (boTap) boSwiped = true;
+            if (tap.Tap) boSwiped = true;
 
-             boTap = false;
-            vec2StartTouchTap = Vector2.zero;
+            tap.Tap = false;
+            tap.Coord = Vector2.zero;
         }
-        #endregion
+#endregion
 
-        #region Mobile Inputs
+#region Mobile Inputs
         if(Input.touches.Length > 0)
         {
             if(Input.touches[0].phase == TouchPhase.Began)
             {
-                if (!boTap) vec2StartTouchTapSwipe = Input.touches[0].position;
+                if (!tap.Tap) vec2StartTouchTapSwipe = Input.touches[0].position;
 
-                boTap = true;
-                vec2StartTouchTap = Input.touches[0].position;
+                tap.Tap = true;
+                tap.Coord = Input.touches[0].position;
             }
             else if (Input.touches[0].phase == TouchPhase.Ended 
                 || Input.touches[0].phase == TouchPhase.Canceled)
             {
-                if (boTap) boSwiped = true;
+                if (tap.Tap) boSwiped = true;
 
-                boTap = false;
-                vec2StartTouchTap = Vector2.zero;
+                tap.Tap = false;
+                tap.Coord = Vector2.zero;
             }
         }
-        #endregion
+#endregion
 
     }
 
 
-    public bool Tap { get { return boTap; } }
-    public Vector2 TapCoordinates { get { return vec2StartTouchTap; } }
+    public StTap Tap { get { return tap; } }
 }
