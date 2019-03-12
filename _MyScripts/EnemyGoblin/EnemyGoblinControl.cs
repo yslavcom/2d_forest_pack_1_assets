359f170b76e31e7774a677c1ee4477892afa9b8a
yslavcom <yslavcom@gmail.com>
2019-03-12 22:48:01 +1100

    - Working on detection of presence

---
+++ _MyScripts/EnemyGoblin/EnemyGoblinControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGoblinControl : MonoBehaviour {
+
+    public float _target_detection_radius = 5;
+    public Transform _graphics;
+
+    public string _target_collider_name = "";
+    public OverlapDetection _overlap_detection;
+
+
+    [SerializeField]
+    private bool _boDetected;
+
+
+    // Use this for initialization
+    void Start () {
+        _boDetected = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var centre = _graphics.transform.position;
+
+        Collider2D[] detected_colliders;
+        _overlap_detection.DetectWithinSphere((Vector2)centre, _target_detection_radius, out detected_colliders);
+        if(null != detected_colliders)
+        {
+            foreach (var collider in detected_colliders)
+            {
+                if(collider.name == _target_collider_name)
+                {
+                    _boDetected = true;
+                }
+            }
+        }
+    }
+}
---
+++ _MyScripts/OverlapDetection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverlapDetection : MonoBehaviour {
+
+	public void DetectWithinSphere(Vector2 centre, float radius, out Collider2D [] detectColliders)
+    {
+        detectColliders = Physics2D.OverlapCircleAll(centre, radius);
+    }
+}
